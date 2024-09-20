0e05b90a2258e18f1eb8174450ac4f5ffe8d1740
XxDaniel-AlzatexX <daniel-alzatem@javeriana.edu.co>
2024-09-20 01:39:59 -0500

    :feat-feat:Creación del proyecto base

---
+++ src/main/java/com/example/ecomarket_servicio_ofertas/EcomarketServicioOfertasApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecomarket_servicio_ofertas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomarketServicioOfertasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomarketServicioOfertasApplication.class, args);
+	}
+
+}
